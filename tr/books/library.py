275a9bad3933ba4add6a58f7bfe2c499815646ec
Istvan Hegyes <hegistva@gmail.com>
2018-05-29 01:06:48 -0400

    First chapters of "Around the world"

--- tr/books/library.py
+++ tr/books/library.py
@@ -1,5 +1,7 @@
 from tr.books.volumes import V_20000LeaguesUnderTheSea
 from tr.books.volumes import V_JourneyToTheCenterOfTheEarth
+from tr.books.volumes import V_AroundTheWorldIn80Days
 
 LIBRARY = {}
 LIBRARY.update(V_20000LeaguesUnderTheSea.VOLUME)
+LIBRARY.update(V_AroundTheWorldIn80Days.VOLUME)
